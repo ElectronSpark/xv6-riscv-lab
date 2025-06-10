eceff9191a84a8c72d5740b7a024ae7f1fc0c94d
electronspark <electronsparkle@gmail.com>
2025-06-10 16:40:01 -0400

    update page unit test wrappers

--- test/include/ut_page_wraps.h
+++ test/include/ut_page_wraps.h
@@ -27,6 +27,7 @@ void __wrap___page_free(page_t *page, uint64 order);
 // Function declarations for real functions that are aliases to the wrappers
 void *__real_page_alloc(uint64 order, uint64 flags);
 void __real_page_free(void *ptr, uint64 order);
+int __real_page_ref_count(page_t *page);
 int __real_page_ref_inc(void *ptr);
 int __real_page_ref_dec(void *ptr);
 int __real_page_refcnt(void *ptr);
@@ -36,10 +37,28 @@ void __real___page_free(page_t *page, uint64 order);
 
 extern bool __wrap___page_alloc_passthrough;
 extern bool __wrap___page_free_passthrough;
+extern bool __wrap_page_alloc_passthrough;
+extern bool __wrap_page_free_passthrough;
+extern bool __wrap_page_ref_inc_passthrough;
+extern bool __wrap_page_ref_dec_passthrough;
+extern bool __wrap_page_refcnt_passthrough;
+extern bool __wrap_page_ref_count_passthrough;
 
 // mock allocating pages with mmap
 page_t *ut_make_mock_page(uint64 order, uint64 flags);
 void ut_destroy_mock_page(void *physical);
 void ut_destroy_mock_page_t(page_t *page);
+
+// Functions to enable/disable page wrapper passthroughs
+void ut_page_wrappers_enable_passthrough(void);
+void ut_page_wrappers_disable_passthrough(void);
+
+// Functions for more granular control of page wrapper passthroughs
+void ut_page_core_alloc_enable_passthrough(void);
+void ut_page_core_alloc_disable_passthrough(void);
+void ut_page_public_alloc_enable_passthrough(void);
+void ut_page_public_alloc_disable_passthrough(void);
+void ut_page_ref_enable_passthrough(void);
+void ut_page_ref_disable_passthrough(void);
 
 #endif // __UT_MOCK_WRAPS_H__
--- test/src/ut_page_wraps.c
+++ test/src/ut_page_wraps.c
@@ -4,6 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Passthrough flags for page wrapper functions
+bool __wrap_page_alloc_passthrough = false;
+bool __wrap_page_free_passthrough = false;
+bool __wrap_page_ref_inc_passthrough = false;
+bool __wrap_page_ref_dec_passthrough = false;
+bool __wrap_page_refcnt_passthrough = false;
+bool __wrap_page_ref_count_passthrough = false;
+
 void __wrap_page_lock_aqcuire(page_t *page) {
     (void)page;
 }
@@ -13,23 +21,35 @@ void __wrap_page_lock_release(page_t *page) {
 }
 
 int __wrap_page_ref_count(page_t *page) {
+    if (__wrap_page_ref_count_passthrough) {
+        return __real_page_ref_count(page);
+    }
     if (page == NULL) return -1;
     return page->ref_count;
 }
 
 int __wrap_page_ref_inc(void *ptr) {
+    if (__wrap_page_ref_inc_passthrough) {
+        return __real_page_ref_inc(ptr);
+    }
     page_t *page = __pa_to_page((uint64)ptr);
     if (page == NULL) return -1;
     return __page_ref_inc(page);
 }
 
 int __wrap_page_ref_dec(void *ptr) {
+    if (__wrap_page_ref_dec_passthrough) {
+        return __real_page_ref_dec(ptr);
+    }
     page_t *page = __pa_to_page((uint64)ptr);
     if (page == NULL) return -1;
     return __page_ref_dec(page);
 }
 
 int __wrap_page_refcnt(void *physical) {
+    if (__wrap_page_refcnt_passthrough) {
+        return __real_page_refcnt(physical);
+    }
     if (physical == NULL) return -1;  // Handle NULL pointer case explicitly
     page_t *page = __pa_to_page((uint64)physical);
     if (page == NULL) return -1;
@@ -63,12 +83,19 @@ void __wrap_kmm_free(void *ptr) {
 }
 
 void *__wrap_page_alloc(uint64 order, uint64 flags) {
+    if (__wrap_page_alloc_passthrough) {
+        return __real_page_alloc(order, flags);
+    }
     page_t *page = __page_alloc(order, flags);
     if (page == NULL) return NULL;
     return (void *)__page_to_pa(page);
 }
 
 void __wrap_page_free(void *ptr, uint64 order) {
+    if (__wrap_page_free_passthrough) {
+        __real_page_free(ptr, order);
+        return;
+    }
     page_t *page = __pa_to_page((uint64)ptr);
     __page_free(page, order);
 }
@@ -136,4 +163,89 @@ void __wrap___page_free(page_t *page, uint64 order) {
     } else {
         mock();
     }
+}
+
+/**
+ * Enable passthrough for all page wrapper functions
+ * This makes all page wrapper functions call their real counterparts
+ */
+void ut_page_wrappers_enable_passthrough(void) {
+    __wrap_page_alloc_passthrough = true;
+    __wrap_page_free_passthrough = true;
+    __wrap_page_ref_inc_passthrough = true;
+    __wrap_page_ref_dec_passthrough = true;
+    __wrap_page_refcnt_passthrough = true;
+    __wrap_page_ref_count_passthrough = true;
+    __wrap___page_alloc_passthrough = true;
+    __wrap___page_free_passthrough = true;
+}
+
+/**
+ * Disable passthrough for all page wrapper functions
+ * This makes all page wrapper functions use mocks
+ */
+void ut_page_wrappers_disable_passthrough(void) {
+    __wrap_page_alloc_passthrough = false;
+    __wrap_page_free_passthrough = false;
+    __wrap_page_ref_inc_passthrough = false;
+    __wrap_page_ref_dec_passthrough = false;
+    __wrap_page_refcnt_passthrough = false;
+    __wrap_page_ref_count_passthrough = false;
+    __wrap___page_alloc_passthrough = false;
+    __wrap___page_free_passthrough = false;
+}
+
+/**
+ * Enable passthrough for core page allocation functions
+ * This enables __page_alloc and __page_free
+ */
+void ut_page_core_alloc_enable_passthrough(void) {
+    __wrap___page_alloc_passthrough = true;
+    __wrap___page_free_passthrough = true;
+}
+
+/**
+ * Disable passthrough for core page allocation functions
+ */
+void ut_page_core_alloc_disable_passthrough(void) {
+    __wrap___page_alloc_passthrough = false;
+    __wrap___page_free_passthrough = false;
+}
+
+/**
+ * Enable passthrough for public page allocation functions
+ * This enables page_alloc and page_free
+ */
+void ut_page_public_alloc_enable_passthrough(void) {
+    __wrap_page_alloc_passthrough = true;
+    __wrap_page_free_passthrough = true;
+}
+
+/**
+ * Disable passthrough for public page allocation functions
+ */
+void ut_page_public_alloc_disable_passthrough(void) {
+    __wrap_page_alloc_passthrough = false;
+    __wrap_page_free_passthrough = false;
+}
+
+/**
+ * Enable passthrough for page reference counting functions
+ * This enables page_ref_inc, page_ref_dec, page_ref_count, and page_refcnt
+ */
+void ut_page_ref_enable_passthrough(void) {
+    __wrap_page_ref_inc_passthrough = true;
+    __wrap_page_ref_dec_passthrough = true;
+    __wrap_page_refcnt_passthrough = true;
+    __wrap_page_ref_count_passthrough = true;
+}
+
+/**
+ * Disable passthrough for page reference counting functions
+ */
+void ut_page_ref_disable_passthrough(void) {
+    __wrap_page_ref_inc_passthrough = false;
+    __wrap_page_ref_dec_passthrough = false;
+    __wrap_page_refcnt_passthrough = false;
+    __wrap_page_ref_count_passthrough = false;
 }
