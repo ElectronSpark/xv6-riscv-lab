755e29855950678705e80e2bedd8c1076ba03061
electronspark <electronsparkle@gmail.com>
2025-07-21 21:39:01 -0400

    signal on top of signal

--- user/testsig.c
+++ user/testsig.c
@@ -2,8 +2,23 @@
 #include "kernel/signo.h"
 #include "user/user.h"
 
+void handler2(int signo) {
+    printf("Signal handler 2 called with signo: %d\n", signo);
+    sigreturn();
+}
+
 void handler1(int signo) {
   printf("Signal handler 1 called with signo: %d\n", signo);
+  sigaction_t sa2 = {0};
+  sa2.handler = handler2;
+  if (sigaction(SIGUSR1, &sa2, NULL) != 0) {
+      fprintf(2, "Failed to set signal handler for SIGUSR1\n");
+      exit(-1);
+  }
+  kill(getpid(), SIGUSR1); // Send SIGUSR1 to self to trigger handler
+  sleep(10); // Give time for the signal to be handled
+  printf("Signal 2 returned\n");
+
   sigreturn();
 }
 
@@ -15,8 +30,7 @@ int main(void) {
         exit(-1);
     }
     kill(getpid(), SIGALRM);
-    sleep(5); // Give time for the signal to be handled
-    sleep(1); // Give time for the signal to be handled
+    sleep(10); // Give time for the signal to be handled
 
     printf("signal returned\n");
 
