70f1c4428caefd60583ffab7a73e8e25d7c1ca86
electronspark <electronsparkle@gmail.com>
2026-01-19 19:40:00 -0500

    print the first 8 PCI devices

--- kernel/pci.c
+++ kernel/pci.c
@@ -13,57 +13,62 @@
 #include "printf.h"
 #include "pci.h"
 
-void
-pci_init()
-{
-  // we'll place the e1000 registers at this address.
-  // vm.c maps this range.
-  uint64 e1000_regs = 0x40000000L;
+void pci_init() {
+    // we'll place the e1000 registers at this address.
+    // vm.c maps this range.
+    uint64 e1000_regs = 0x40000000L;
 
-  // qemu -machine virt puts PCIe config space here.
-  // vm.c maps this range.
-  uint32  *ecam = (uint32 *) 0x30000000L;
-  
-  if (sizeof(struct pci_common_confspace_header) != 0x40) {
-    printf("sizeof pci_common_confspace_header: %lx\n", sizeof(struct pci_common_confspace_header));
-    panic("The size of PCI-E Common Configuration Space Header Structure is not 0x40 Bytes!");
-  }
+    // qemu -machine virt puts PCIe config space here.
+    // vm.c maps this range.
+    uint32 *ecam = (uint32 *)0x30000000L;
 
-  // look at each possible PCI device on bus 0.
-  for(int dev = 0; dev < 32; dev++){
-    int bus = 0;
-    int func = 0;
-    int offset = 0;
-    uint32 off = (bus << 16) | (dev << 11) | (func << 8) | (offset);
-    volatile uint32 *base = ecam + off;
-    volatile struct pci_common_confspace_header *dsc = (void *)base;
-    
-    // 100e:8086 is an e1000
-    if(dsc->device_id == 0x100e && dsc->vendor_id == 0x8086){
-      printf("E1000 Ethernet Controller detected.\n");
-      // command and status register.
-      // bit 0 : I/O access enable
-      // bit 1 : memory access enable
-      // bit 2 : enable mastering
-      dsc->command = PCIE_CSCMD_IAE | PCIE_CSCMD_MAE | PCIE_CSCMD_BME;
-      __atomic_thread_fence(__ATOMIC_SEQ_CST);
-
-      for(int i = 0; i < 6; i++){
-        uint32 old = dsc->header_type_0.base_addr[i];
-
-        // writing all 1's to the BAR causes it to be
-        // replaced with its size.
-        dsc->header_type_0.base_addr[i] = 0xffffffff;
-        __atomic_thread_fence(__ATOMIC_SEQ_CST);
-
-        dsc->header_type_0.base_addr[i] = old;
-      }
-
-      // tell the e1000 to reveal its registers at
-      // physical address 0x40000000.
-      dsc->header_type_0.base_addr[0] = e1000_regs;
-
-      e1000_init((uint32*)e1000_regs);
+    if (sizeof(struct pci_common_confspace_header) != 0x40) {
+        printf("sizeof pci_common_confspace_header: %lx\n",
+               sizeof(struct pci_common_confspace_header));
+        panic("The size of PCI-E Common Configuration Space Header Structure "
+              "is not 0x40 Bytes!");
     }
-  }
+
+    // look at each possible PCI device on bus 0.
+    for (int dev = 0; dev < 32; dev++) {
+        int bus = 0;
+        int func = 0;
+        int offset = 0;
+        uint32 off = (bus << 16) | (dev << 11) | (func << 8) | (offset);
+        volatile uint32 *base = ecam + off;
+        volatile struct pci_common_confspace_header *dsc = (void *)base;
+
+        if (dev < 8) {
+            printf("PCI device %d:%d:%d - vendor ID: 0x%x, device ID: 0x%x\n",
+                   bus, dev, func, dsc->vendor_id, dsc->device_id);
+        }
+
+        // 100e:8086 is an e1000
+        if (dsc->device_id == 0x100e && dsc->vendor_id == 0x8086) {
+            printf("E1000 Ethernet Controller detected.\n");
+            // command and status register.
+            // bit 0 : I/O access enable
+            // bit 1 : memory access enable
+            // bit 2 : enable mastering
+            dsc->command = PCIE_CSCMD_IAE | PCIE_CSCMD_MAE | PCIE_CSCMD_BME;
+            __atomic_thread_fence(__ATOMIC_SEQ_CST);
+
+            for (int i = 0; i < 6; i++) {
+                uint32 old = dsc->header_type_0.base_addr[i];
+
+                // writing all 1's to the BAR causes it to be
+                // replaced with its size.
+                dsc->header_type_0.base_addr[i] = 0xffffffff;
+                __atomic_thread_fence(__ATOMIC_SEQ_CST);
+
+                dsc->header_type_0.base_addr[i] = old;
+            }
+
+            // tell the e1000 to reveal its registers at
+            // physical address 0x40000000.
+            dsc->header_type_0.base_addr[0] = e1000_regs;
+
+            e1000_init((uint32 *)e1000_regs);
+        }
+    }
 }
